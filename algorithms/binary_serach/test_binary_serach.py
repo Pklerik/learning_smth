7694c411a13a2d89133b4d1a5cd9d1b0e41a884c
Pavel Budkov <pklerik@mail.ru>
2024-04-07 15:44:45 +0300

    [fix] - added test for strings

--- algorithms/binary_serach/test_binary_serach.py
+++ algorithms/binary_serach/test_binary_serach.py
@@ -9,3 +9,9 @@ def test_binary_search_case_2():
     sorted_list:List = [i for i in range(1, 100)]
     search_value = 4
     assert binary_search.simple_binary_search(sorted_list, search_value) == 3
+    
+def test_binary_search_case_4():
+    import string
+    sorted_list:List = [i for i in string.ascii_lowercase]
+    search_value = 'c'
+    assert binary_search.simple_binary_search(sorted_list, search_value) == 2
